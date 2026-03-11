9960645d3e993d0d99fd2b0d10e9d2c6b4f67379
Teu Nome <teuemail@exemplo.com>
2026-03-11 23:07:30 +0200

    Fix React Three Fiber types

--- src/@types/react-three-fiber.d.ts
+++ src/@types/react-three-fiber.d.ts
@@ -1,12 +1,1 @@
-import 'react';
-import { MeshStandardMaterialProps } from '@react-three/fiber';
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      meshStandardMaterial: MeshStandardMaterialProps;
-      // se precisar, pode adicionar outros materiais, ex:
-      // meshBasicMaterial: MeshBasicMaterialProps;
-    }
-  }
-}
+import '@react-three/fiber'
